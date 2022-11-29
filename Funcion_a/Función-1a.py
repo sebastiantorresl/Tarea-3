b6abcfc284b3d487302b6901c49b1fb9dc5a69c4
sebastiantorresl <sebastian_torres_lopez@hotmail.com>
2022-11-29 05:52:39 -0600

    Solución al inciso a del ejercicio 1 de la tarea

---
+++ Funcion_a/Función-1a.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# el dominio de la funcion es [-1,1]
+x = tf.linspace(-1,1,500)
+y = 3*np.sin(np.pi*x) #Funcion a reproducir
+
+model = Sequential()
+model.add(Dense(20, activation='tanh',input_shape=(1,)))
+model.add(Dense(20, activation ='tanh'))
+model.add(Dense(1, activation = 'linear'))
+
+model.summary()
+
+model.compile(optimizer=RMSprop(),loss='mse')
+
+history = model.fit(x,y,epochs=500,batch_size=10)
+
+f = model.predict(x)
+plt.plot(x,f,label='$f(x)$ de la red',color = 'b')
+plt.plot(x,y,label='$f(x)$',color = 'r',ls='--')
+plt.grid()
+plt.legend()
+plt.show()
+model.save('red1a_T3.h5')
+
+modelo_cargado = tf.keras.models.load_model("red1a_T3.h5")   
